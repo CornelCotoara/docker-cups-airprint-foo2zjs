a1c008184bbadd2d9dde109ab091190316709ef4
CornelCotoara <94889846+CornelCotoara@users.noreply.github.com>
2022-12-04 15:08:28 +0200

    Update build-foo2zjs.sh

--- scripts/build-foo2zjs.sh
+++ scripts/build-foo2zjs.sh
@@ -2,8 +2,8 @@
 	git -c http.sslVerify=false clone https://github.com/koenkooi/foo2zjs.git &&
 	cd foo2zjs &&
 	make &&
-	./getweb all &&
+	# ./getweb all && # already available via git clone
 	make install &&
 	make install-hotplug &&
 	cd .. &&
-	rm -rf foo2zjs.tar.gz foo2zjs
+	rm -rf foo2zjs
