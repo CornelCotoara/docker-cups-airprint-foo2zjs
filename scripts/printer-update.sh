d53e803ec05ad2af8f8f287868fe12a315fff374
CornelCotoara <94889846+CornelCotoara@users.noreply.github.com>
2022-12-05 17:38:05 +0200

    Update printer-update.sh

--- scripts/printer-update.sh
+++ scripts/printer-update.sh
@@ -2,8 +2,6 @@
 inotifywait -m -e close_write,moved_to,create /etc/cups |
 	while read -r directory events filename; do
 		if [ "$filename" = "printers.conf" ]; then
-			rm -rf /services/AirPrint-*.service
-			/scripts/airprint-generate.py -d /services
 			cp /etc/cups/printers.conf /config/printers.conf
 		fi
 	done
