b41e1cc600faa724c3c3745d50ab01469202cff1
CornelCotoara <94889846+CornelCotoara@users.noreply.github.com>
2022-12-05 18:11:07 +0200

    Update send_firmware.sh

--- scripts/send_firmware.sh
+++ scripts/send_firmware.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-inotifywait -m -e create /hp1020 |
+inotifywait -m -e close_write,moved_to,create,attrib,access /hp1020 |
         while read -r directory events filename; do
                 if [ "$filename" = "need_fw" ]; then
                         ./etc/hotplug/usb/hplj1020
                         touch /hp1020/succes 
-                        rm /hp1020/cat_fw
+                        rm /hp1020/need_fw
                 fi
         done
